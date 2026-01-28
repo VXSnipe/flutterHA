dadfc77ba8dacb11c4c6afd2580c619fd82d5f2b
VXSnipe <mattsheld01@gmail.com>
2026-01-28 07:52:18 +0100

    Integrated local notification service

---
+++ lib/notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService {
+  final FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  Future<void> initNotification() async {
+    AndroidInitializationSettings initializationSettingsAndroid = const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
+    await notificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> showNotification({String? title, String? body}) async {
+    return notificationsPlugin.show(
+      0, title, body,
+      const NotificationDetails(
+        android: AndroidNotificationDetails('channelId', 'channelName', importance: Importance.max),
+      ),
+    );
+  }
+}
